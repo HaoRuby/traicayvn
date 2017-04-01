83c7ee0d4c58bcd1ff8839d4c29b952addff4bb9
Hao Nguyen <hao@localhost.localdomain>
2017-04-01 21:27:54 +0700

    show logo

--- app/models/spree/store_info.rb
+++ app/models/spree/store_info.rb
@@ -19,4 +19,13 @@ class Spree::StoreInfo < ActiveRecord::Base
 										default_style:  "medium"
 	validates_attachment :logo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
+=begin :old
+has_attached_file :logo,
+										styles: { medium: "300x50>", thumb: "100x100>" },
+                    url: '/spree/store_infos/:id/:style/:basename.:extension',
+                    path: ':rails_root/public/spree/store_infos/:id/:style/:basename.:extension',
+                    convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
+validates_attachment :logo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
+=end
 end
