d501518701eac7c2eaf63d5d565efd32afe2b469
Hao Nguyen <hao@localhost.localdomain>
2017-04-03 08:28:27 +0700

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-#Spree::Core::Engine.load_seed if defined?(Spree::Core)
-#Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
+Spree::Core::Engine.load_seed if defined?(Spree::Core)
+Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
 
-Spree::StoreInfo.create(address: '06 Le Loi street', phone: '054 3 567 678', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore eveniet a cum, ad asperiores reprehenderit eius quas laborum dicta! Voluptatem excepturi distinctio, at ea non temporibus, amet? Velit, adipisci, omnis!')
+#Spree::StoreInfo.create(address: '06 Le Loi street', phone: '054 3 567 678', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore eveniet a cum, ad asperiores reprehenderit eius quas laborum dicta! Voluptatem excepturi distinctio, at ea non temporibus, amet? Velit, adipisci, omnis!')
