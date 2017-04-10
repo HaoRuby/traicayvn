4bf74cdc4e69e4ccfe5cd0c46a32cd754d6d417d
Hao Nguyen <hao@localhost.localdomain>
2017-04-10 08:27:04 +0700

    sent mail

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,16 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.default_url_options = { host:'https://traicayvn.herokuapp.com'}  
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.raise_delivery_errors = true  
+  config.action_mailer.default :charset => "utf-8"  
+  config.action_mailer.smtp_settings = {      
+                                          :address => "smtp.gmail.com",      
+                                          :port => 587,      
+                                          :user_name => "haonguyen4993@gmail.com",      
+                                          :password => "hanhdong6090",      
+                                          :authentication => :plain,      
+                                          :enable_starttls_auto => true  }
 end
